ea8d663c45d9e238c46e4f0fd773f55185de18b4
edc2k19 <54032273+edc2k19@users.noreply.github.com>
2022-02-21 11:43:03 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "token": "OTQ0ODQ4MzIxMDM2NTgyOTMy.YhHkkg.UEvx2UHKQUix_nOvKZqSkdcebss",
+    "prefix": "..",
+    "MEMER_API_TOKEN": "", // Get The Memer API Token from https://discord.com/invite/emD44ZJaSA
+    "TENOR_API_KEY": "ABOIWKGCRD1A", // Get The Tenor API Key from https://www.tenor.co/api/v1/key/
+    "mongooseConnectionString": "mongodb+srv://fwworld:fw2125fw@murashka-bot.sazpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
+    "id": "944848321036582932",  // https://discordapp.com/developers/applications/ID/information,
+    "clientSecret": "KtiN9pxGepJ02KS6_qArc4iL8q53TeLD",  // https://discordapp.com/developers/applications/ID/information,
+    "domain": "http://localhost:3000",
+    "port": 3000,
+    "usingCustomDomain": false
+}
+
+/**
+ * !!!
+ * You need to add a redirect url to https://discordapp.com/developers/applications/ID/oauth2.
+ * Format is: domain:port/callback example http://localhost:8080/callback
+ * - Do not include port if the port is 80.
+ * !!!
+ */
+
+
