b41d2e16e039040d6b64c3f0cf9ac37c720b64af
edc2k19 <54032273+edc2k19@users.noreply.github.com>
2022-02-21 11:46:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    "token": "OTQ0ODQ4MzIxMDM2NTgyOTMy.YhHkkg.UEvx2UHKQUix_nOvKZqSkdcebss",
+    "token": "",
     "prefix": "..",
     "MEMER_API_TOKEN": "", // Get The Memer API Token from https://discord.com/invite/emD44ZJaSA
-    "TENOR_API_KEY": "ABOIWKGCRD1A", // Get The Tenor API Key from https://www.tenor.co/api/v1/key/
-    "mongooseConnectionString": "mongodb+srv://fwworld:fw2125fw@murashka-bot.sazpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
-    "id": "944848321036582932",  // https://discordapp.com/developers/applications/ID/information,
-    "clientSecret": "KtiN9pxGepJ02KS6_qArc4iL8q53TeLD",  // https://discordapp.com/developers/applications/ID/information,
+    "TENOR_API_KEY": "", // Get The Tenor API Key from https://www.tenor.co/api/v1/key/
+    "mongooseConnectionString": "mongodb+srv://:@-bot.sazpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
+    "id": "",  // https://discordapp.com/developers/applications/ID/information,
+    "clientSecret": "",  // https://discordapp.com/developers/applications/ID/information,
     "domain": "http://localhost:3000",
     "port": 3000,
     "usingCustomDomain": false
